3491270c0320609a36410f1f9755ac6a403a95fa
shawnfa_cp <SND\shawnfa_cp>
2008-08-25 19:19:21 +0000

    Add the RSAPKCS1SHA256SignatureDescription class.
    This class allows the XML digital signature stack access to the .NET 3.5 SP 1 RSA-SHA256 signature feature. It requires .NET 3.5 SP 1 and Windows Server 2003 or higher to work.  It also needs to be registered in the machine wide CryptoConfig in order to be visible to SignedXml.

---
+++ Security.Cryptography/src/RSAPKCS1SHA256SignatureDescription.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+
+namespace Security.Cryptography
+{
+    /// <summary>
+    ///     Signature description type for the http://www.w3.org/2001/04/xmldsig-more#rsa-sha256 signature
+    ///     type. Registering this type allows XML digitial signatures to be created and verified using
+    ///     RSA-SHA256 signatures.
+    ///     
+    ///     This description can be registered in machine.config in a section similar to:
+    ///     <configuration>
+    ///       <mscorlib>
+    ///         <cryptographySettings>
+    ///           <cryptoNameMapping>
+    ///             <cryptoClasses>
+    ///               <cryptoClass RSASHA256SignatureDescription="Security.Cryptography.RSAPKCS1SHA256SignatureDescription, Security.Cryptography, Version=1.1.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" />
+    ///             </cryptoClasses>
+    ///             <nameEntry name="http://www.w3.org/2001/04/xmldsig-more#rsa-sha256" class="RSASHA256SignatureDescription" />
+    ///           </cryptoNameMapping>
+    ///         </cryptographySettings>
+    ///       </mscorlib>
+    ///     </configuration>
+    ///     
+    ///     Where the assembly name is update to refer to the assembly the signature description is hosted in.
+    ///     
+    ///     This signature description type requires .NET 3.5 SP 1 and Windows 2003 or higher in order to work.
+    /// </summary>
+    [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "RSAPKCS", Justification = "This casing is to match the existing RSAPKCS1SHA1SignatureDescription type")]
+    [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "SHA", Justification = "This casing is to match the use of SHA throughout the framework")]
+    public sealed class RSAPKCS1SHA256SignatureDescription : SignatureDescription
+    {
+        public RSAPKCS1SHA256SignatureDescription()
+        {
+            KeyAlgorithm = typeof(RSACryptoServiceProvider).FullName;
+            DigestAlgorithm = typeof(SHA256Managed).FullName;   // Note - SHA256CryptoServiceProvider is not registered with CryptoConfig
+            FormatterAlgorithm = typeof (RSAPKCS1SignatureFormatter).FullName;
+            DeformatterAlgorithm = typeof(RSAPKCS1SignatureDeformatter).FullName;
+        }
+
+        public override AsymmetricSignatureDeformatter CreateDeformatter(AsymmetricAlgorithm key)
+        {
+            if (key == null)
+                throw new ArgumentNullException("key");
+
+            RSAPKCS1SignatureDeformatter deformatter = new RSAPKCS1SignatureDeformatter(key);
+            deformatter.SetHashAlgorithm("SHA256");
+            return deformatter;
+        }
+
+        public override AsymmetricSignatureFormatter CreateFormatter(AsymmetricAlgorithm key)
+        {
+            if (key == null)
+                throw new ArgumentNullException("key");
+
+            RSAPKCS1SignatureFormatter formatter = new RSAPKCS1SignatureFormatter(key);
+            formatter.SetHashAlgorithm("SHA256");
+            return formatter;
+        }
+    }
+}
+
---
+++ Security.Cryptography/test/RSAPKCS1SHA256SignatureDescriptionTests.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Security.Cryptography;
+
+namespace Security.Cryptography.Test
+{
+    /// <summary>
+    ///     Test cases for the RSAPKCS1SHA256SignatureDescription class
+    /// </summary>
+    [TestClass]
+    public sealed class RSAPKCS1SHA256SignatureDescriptionTests
+    {
+        /// <summary>
+        ///     Test to ensure that the properties of the signature description contain the expected values
+        /// </summary>
+        [TestMethod]
+        public void RSAPKCS1SHA256SignatureDescriptionPropertiesTest()
+        {
+            RSAPKCS1SHA256SignatureDescription sd = new RSAPKCS1SHA256SignatureDescription();
+
+            // We should be using RSAPKCS1 for formatting
+            Assert.AreEqual(typeof(RSAPKCS1SignatureDeformatter).FullName, sd.DeformatterAlgorithm);
+            Assert.AreEqual(typeof(RSAPKCS1SignatureFormatter).FullName, sd.FormatterAlgorithm);
+
+            // We should be using SHA256 as the digest algorithm
+            Assert.AreEqual(typeof(SHA256Managed).FullName, sd.DigestAlgorithm);
+            using (HashAlgorithm digestAlgorithm = (HashAlgorithm)CryptoConfig.CreateFromName(sd.DigestAlgorithm))
+            {
+                Assert.IsInstanceOfType(digestAlgorithm, typeof(SHA256Managed));
+            }
+
+            // We should be using RSACryptoServiceProvider as the signature algorithm
+            Assert.AreEqual(typeof(RSACryptoServiceProvider).FullName, sd.KeyAlgorithm);
+            using (AsymmetricAlgorithm keyAlgorithm = (AsymmetricAlgorithm)CryptoConfig.CreateFromName(sd.KeyAlgorithm))
+            {
+                Assert.IsInstanceOfType(keyAlgorithm, typeof(RSACryptoServiceProvider));
+            }
+        }
+
+        /// <summary>
+        ///     Test to ensure that the creation methods of the signature description create the expected types
+        /// </summary>
+        [TestMethod]
+        public void RSAPKCS1SHA256SignatureCreateTest()
+        {
+            RSAPKCS1SHA256SignatureDescription sd = new RSAPKCS1SHA256SignatureDescription();
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                // We should be using the PKCS1 signature deformatter
+                AsymmetricSignatureDeformatter deformatter = sd.CreateDeformatter(rsa);
+                Assert.IsInstanceOfType(deformatter, typeof(RSAPKCS1SignatureDeformatter));
+
+                // We should be using the PKCS1 signature formatter
+                AsymmetricSignatureFormatter formatter = sd.CreateFormatter(rsa);
+                Assert.IsInstanceOfType(formatter, typeof(RSAPKCS1SignatureFormatter));
+            }
+
+            // We should be using SHA256Managed for hashing
+            using (HashAlgorithm digestAlgorithm = sd.CreateDigest())
+            {
+                Assert.IsInstanceOfType(digestAlgorithm, typeof(SHA256Managed));
+            }
+        }
+
+        /// <summary>
+        ///     Test to ensure that we can sign and verify a message through the signature description
+        /// </summary>
+        [TestMethod]
+        public void RSAPKCS1SHA256RoundTripTest()
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048))
+            {
+                RSAPKCS1SHA256SignatureDescription sd = new RSAPKCS1SHA256SignatureDescription();
+
+                using (HashAlgorithm digestAlgorithm = sd.CreateDigest())
+                {
+                    // Create some data to sign
+                    RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+                    byte[] data = new byte[1025];
+                    rng.GetBytes(data);
+                    byte[] hash = digestAlgorithm.ComputeHash(data);
+
+                    // Sign the data
+                    AsymmetricSignatureFormatter formatter = sd.CreateFormatter(rsa);
+                    byte[] signature = formatter.CreateSignature(hash);
+                    Assert.IsNotNull(signature, "Failed to create a signature");
+
+                    // Verify the signature
+                    AsymmetricSignatureDeformatter deformatter = sd.CreateDeformatter(rsa);
+                    Assert.IsTrue(deformatter.VerifySignature(hash, signature), "Failed to verify signature");
+                }
+            }
+        }
+    }
+}
